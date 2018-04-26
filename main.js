2c89701d094fbea883395d358bd52ecc700e3a99
Valerija Vukoje <valerija.vukoje@gmail.com>
2018-04-26 15:12:52 +0200

    Update main.js
    Add function syntaxErr(), improve function test()

--- main.js
+++ main.js
@@ -1,175 +1,159 @@
+/*jshint esversion: 6 */
+/* jshint node: true */
+/*jslint evil: true */
 'use strict';
 
 const display = document.getElementById('display');
 const dot = document.getElementById('dot');
 let lastElement = 'operator';
 
-//Adds numbers to display.
+//Add numbers to display.
 function addNumber(id) {
-  let num = document.getElementById(id);
-  let number = num.textContent;
+  let number = document.getElementById(id).textContent;
   let val = display.value;
-  let n = parseInt(number)
-   console.log(typeof n)
   //Removes leading zero if no decimal.
-  if (val[0] === '0' && val[1] !== '.') {
-    val = val.slice(1);
-  }
-    
-  if (val.length <= 17) {//11) {
-    val += n.toLocaleString('en-US')
-  }
-  lastElement = 'number'; 
-  display.value = val;
-  console.log(lastElement +'|' + display.value);
- }
-
-//Adds operators to a display.
-function addOperator(id) {
-  let opr = document.getElementById(id);
-  let operator = opr.textContent;
-  let val = display.value;
-  let len = val.length - 1;
-  let last;
-  
-  // Prevents a first char to be an operator.
-  if (1 <= val.length < 16 && lastElement !== 'operator') {
-    
+  if (val[0] === '0'&& val[1] !== '.') {
+    val = val.slice(0, -1);
     lastElement = 'operator';
-    val += operator;
   }
-  //Changes operator
-  else if (1 <= val.length < 16 && lastElement === 'operator') {
-    //Converts "x" to "*" and "÷" to "/".
-    if (operator === 'x') {
-      last = val.substring(0, len) + '*';
-    }
-    else if (operator === '÷') {
-      last = val.substring(0, len) + '/';
-    } 
-
-    last = val.substring(0, len) + operator;
-    val = last; 
+  if (val.length < 17) {
+    val += number;
+    lastElement = 'number'; 
   }
-  console.log(val.length); 
   display.value = val;
-  console.log(lastElement +'|' + display.value);
-  }
+  console.log(`${lastElement} ${display.value}`);
+}
+
+
+function addOperator(id) {
+  let operator = document.getElementById(id).textContent;
+  let val = display.value;
+  // Prevents a first char to be an operator.
+  if (1 <= val.length && val.length < 16) {
+    if (lastElement === 'number') {
+       val += operator;
+       lastElement = 'operator';
+    }
+    //Changes operator
+    else if (lastElement === 'operator') {
+      val = val.slice(0, -1) + operator;
+    }
+  }  
+  display.value = val;
+  console.log(`${lastElement} ${display.value}`);
+}
 
 //Set backspace functionality.
 function setBackspace() {
    let val = display.value;
-   let len = val.length - 1;
-   let backspace = val.substring(0, len);
-   display.value = backspace; 
+   val = val.slice(0, -1);
+   display.value = val; 
 }
 
-//Deletes all.
+//Delete all.
 function setDeleteAll() {
   display.value = '';
   lastElement = 'operator';
-
+  
   if (dot.classList.contains('unclickable')) {
-    dot.classList.remove('unclickable');
-    dot.classList.add('clickable');
+    dot.classList.remove('unclickable').add('clickable')
   } else {
     return;
-  }
+   }
 }
 
 //Change positive number to negativ and vice versa.
 function switchSign() {
   let val = display.value;
-
-  if(val[0] === '-') {
-    val = val.slice(1);
-  } else {
-    val = '-' + val.slice(0);
-  }
+  val = val[0] === '-' ? val.slice(1) : '-' + val.slice(0);
   display.value = val;
 }
 
 function setSquare() {
   let val = display.value;
- //Squares a value and replaces decimal zeros if exist.   
+//Squares a value and replaces decimal zeros if exist.   
   if (val.length <= 13) {
-    val = Math.pow(val, 2).toFixed(2).replace(/\.?0*$/g,'').toLocaleString('en-US');
-  } 
-  else if (val.length > 13) {
+    val = Math.pow(val, 2).toFixed(4).replace(/\.?0*$/g,'');
+  } else {
     val = expo(Math.pow(val, 2), 6);
-  } 
+    
+    if(val === 'Infinity') {
+      val = test(val);
+    }
+  }
   display.value = val;
-  console.log(test(display.value));
 }
 
 function setTotal() {
   let val = display.value;
-  //If last element operator display Err.
-  if (lastElement === 'operator') {
-    display.value = 'Err';
-    return display.value; 
-    }
-  //Calculates a value and replaces decimal zeros if exist.  
-  else if (val.length <= 17) {
-   val = parseInt(eval(val)).toFixed(2).replace(/\.?0*$/g,'').toLocaleString('en-US');
+  //If last element operator display Error.
+  if(lastElement === 'operator') {
+    return syntaxErr(val); 
+  }
+  //Calculate a value and replace decimal zeros if exist.  
+  if (val.length <= 17) {
+    val = eval(val).toFixed(4).replace(/\.?0*$/g,'');
+   
   } 
-  else if (val.length > 17) {
+  else {
     val = expo(eval(val), 6);
-  } 
+  }
+  
+  val = test(val);
   display.value = val;
-  console.log(test(display.value));
 }
 
-/*Returns a string representing the number object in exponential notation.*/
-function expo(x, f) {
-  return Number.parseFloat(x).toExponential(f);
+//Return a string representing the number object in exponential notation.
+const expo = (x, f) => Number.parseFloat(x).toExponential(f);
+/*Exception handling statements 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling#Exception_handling_statements*/
+function syntaxErr() {
+  try {
+    throw new SyntaxError('Error');
+  } catch (e) {
+    alert(`${e.name}: Unexpected end of input`);
+    display.value = e.message;
+    return display.value; 
+    }    
+}
+/*Determines whether the passed value is a finite number. 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isFinite*/
+function test(number) {
+  //number = parseInt(number);
+
+  if (Number.isFinite(number)) {
+    console.log('Number is NOT Infinity.');
+    return number;//Return if a value is NOT Infinity, -Infinity, NaN, null or undefined.
+    }
+ 
+  try {
+    throw new Error('Error');
+  } catch (e) {
+    console.log(`${e.name}: ${e.message}`);
+     
+    if (typeof number === 'undefined') {
+      alert('Value is undefined!');
+      return e.message;//Return true if a value is undefined.
+    }
+    alert('Value is Infinity!');
+    return e.message;//Return if a value is Infinity, -Infinity, NaN or null.
+    }
 }
 
-function test() {
-  if (isFinite(display.value)) {
-    return display.value;//Value is NOT Infinity.
-  }
-
-  else if (isNaN(display.value)) {
-    display.value = 'Err';
-    return display.value;//Returns if a value is NaN.
-  }
-
-  else if (typeof display.value === 'undefined') {
-    display.value = 'Err';
-    return display.value;//Returns if a value undefined
-  }
-
-  else if (display.value === null) {
-    display.value = 'Err';
-    return display.value;//Returns if a value null.
- }
-
-  else if (display.value instanceof Error && typeof display.message !== 'undefined'){
-    display.value = 'Err';
-    return display.value;// Returns if value is an error object
- }
-
-  display.value = 'Err';
-  return display.value;//Value is Infinity!
- }
-
-//Prevents entering charachters.
-display.addEventListener('keydown', (e) => {
-  e.preventDefault();
-});
+//Prevent enter charachters.
+display.addEventListener('keydown', e => e.preventDefault());
   
 //Add event delegation to event listener. 
-document.addEventListener('click', (e) => {
+document.addEventListener('click', e => {
   
   const clicked = e.target;
   
   //If error, prevents clicking buttons except ac button.
-  if (display.value === 'Err') {
+  if (display.value === 'Error') {
     if (clicked.id === 'ac') {
       setDeleteAll();
     } else {
-      return false;
+      return;
      }
   }  
 
@@ -178,7 +162,7 @@ document.addEventListener('click', (e) => {
     }
  
   if (clicked.classList[0] === 'operator') {
-   //Prevents multiple decimals in one number.
+   //Prevent multiple dots in one number.
     if (clicked === dot) {
       dot.classList.remove('clickable');
       dot.classList.add('unclickable');
